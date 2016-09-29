51f9de20be6f63f8fa52ed9a7489589215cc33e1
Louise Yang <louise.m.yang@gmail.com>
2016-09-29 11:59:00 -0700

    Refactored to use promises to avoid pyramid of death, somewhat. Fixed a bug with user's first name not appearing in the email.

--- handler.js
+++ handler.js
@@ -20,57 +20,72 @@ const kmsParams = {
 const emailSubject = 'Welcome to KPCC Plus!';
 const startingTime = new Date().getTime();
 
-module.exports.sendEmails = (event, context, cb) => {
-  kms.decrypt(kmsParams, (err, data) => {
-    if (err) {
-      console.log(err, err.stack)
-    } else {
-      const secrets = YAML.safeLoad(data['Plaintext'].toString());
-      mailClient = new postmark.Client(secrets.postmarkKey);
+module.exports.sendEmails = () => {
+  new Promise((resolve, reject) => {
+    kms.decrypt(kmsParams, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  }).then((data) => {
+    const secrets = YAML.safeLoad(data['Plaintext'].toString());
+    mailClient = new postmark.Client(secrets.postmarkKey);
+
+    return new Promise((resolve, reject) => {
       MongoClient.connect(secrets.mongoDbUri, (err, db) => {
         if (err) {
-          console.log('Unable to connect to mongoDB server. Error:', err);
+          reject(err);
         } else {
-          const collection = db.collection('PfsUser');
-          collection.find({emailSent: { $ne: true }}).toArray((err, results) => {
-            if (err) {
-              console.log(err);
-            }  else if (results.length) {
-              _.each(results, (result) => {
-                sendIndividualEmail(result, {
-                  success: () => {
-                    collection.updateOne(
-                      { "_id": result._id },
-                      {$set: { emailSent: true }}
-                    );
-                    handleResult();
-                  },
-                  error: (error) => {
-                    console.log(error);
-                    handleResult();
-                  }
-                });
-              });
-              const handleResult = _.after(results.length, () => {
-                const elapsedTime = new Date().getTime() - startingTime;
-                console.log(`Attempted to send ${results.length} emails in ${elapsedTime}ms`);
-                console.log(`Successfully sent ${sentEmails} emails`);
-                db.close();
-              });
-            } else {
-              console.log('No users to email');
-              db.close();
-            }
-          });
+          resolve(db);
         }
       });
-    }
-  });
+    });
+  })
+    .then((db) => {
+      const collection = db.collection('PfsUser');
+      collection.find({emailSent: { $ne: true }}).toArray((err, results) => {
+        if (err) {
+          console.log(err);
+        }  else if (results.length) {
+          _.each(results, (result) => {
+            const sendIndividualEmailCallback = {
+              success: () => {
+                collection.updateOne(
+                  { "_id": result._id },
+                  {$set: { emailSent: true }}
+                );
+                handleResult();
+              },
+              error: (error) => {
+                console.log(error);
+                handleResult();
+              }
+            };
+            sendIndividualEmail(result, sendIndividualEmailCallback);
+          });
+          const handleResult = _.after(results.length, () => {
+            const elapsedTime = new Date().getTime() - startingTime;
+            console.log(`Attempted to send ${results.length} emails in ${elapsedTime}ms`);
+            console.log(`Successfully sent ${sentEmails} emails`);
+            db.close();
+          });
+        } else {
+          console.log('No users to email');
+          db.close();
+        }
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
+
 
 function sendIndividualEmail(userObject, callback) {
 
-  const emailBody = `Dear ${userObject.name}, <br/><p>Congratulations! You can now stream KPCC on your computer 
+  const emailBody = `Dear ${userObject.firstName}, <br/><p>Congratulations! You can now stream KPCC on your computer 
 or mobile device during our member drives - without any fundraising interruptions.</p><p>The fundraising-free stream 
 KPCC Plus is easy to access. Click or paste this link to listen on your desktop or mobile web browser: 
 http://www.scpr.org/listen_live/pledge-free?pledgeToken=${userObject.pledgeToken}</p><p>You can also access 
@@ -80,7 +95,7 @@ screen. Choose the KPCC Plus stream from the menu, and type this code:${userObje
  stories you love.</p><p></br><br/>Sincerely, <br/>Rob Risko</p><p>P.S. Having trouble accessing KPCC Plus? Call us at
   626-583-5121 or visit our FAQ page: http://www.kpcc.org/plus</p>`;
 
-  const email = 'louise.yang@scpr.org'; //userObject.email;
+  const email = 'louise.yang@scpr.org'; // TODO: deploy to prod with: userObject.email;
   mailClient.sendEmail({
     To: email,
     From: 'Rob Risko <membership@kpcc.org>',
@@ -92,7 +107,7 @@ screen. Choose the KPCC Plus stream from the menu, and type this code:${userObje
         callback.error('Failed to send an email');
         return;
       }
-      console.log('Email sent to ' + userObject.email);
+      console.log('Email sent to ' + email);
       sentEmails = sentEmails + 1;
       callback.success(true);
     }
